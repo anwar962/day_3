1f581e9038b541347b92da5564479f00350ab528
anwar962 <elmousaouyanwar31@gmail.com>
2025-09-03 18:49:56 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+float base;
+int exposant;
+
+    printf("saisi une base:");
+    scanf("%f",&base);
+    printf("saisi un exposant:");
+    scanf("%d",&base);
+    base=1;
+    if(exposant==0){
+    printf("le nombre est :1);
+           }else
+    for(i=1;i<=exposant;i++){
+        base=base*n;}
+    printf("%d^%d=%d",n,exposant,base)}
+
+
+    return 0;
+}
